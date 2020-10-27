e47ed6796250cf50673458cd60d953cd34676a65
Laurent Arthur <laurent.arthur75@gmail.com>
2020-10-27 15:52:42 +0100

    Add prefix handling on redis adapter

--- adapters/redis.py
+++ adapters/redis.py
@@ -6,17 +6,25 @@ logger = logging.getLogger(__name__)
 
 
 class RedisAdapter(object):
-    def __init__(self, **kwargs):
+    def __init__(self, prefix=None, **kwargs):
         self.redis = StrictRedis(**kwargs)
+        self.prefix = f"{prefix}:" if prefix else ""
+
+    def _set_prefix(self, value):
+        return f"{self.prefix}{value}"
 
     def set(self, key, value, **kwargs):
-        return self.redis.set(key, value, **kwargs)
+        return self.redis.set(self._set_prefix(key), value, **kwargs)
 
     def get(self, key):
-        return self.redis.get(key)
+        return self.redis.get(self._set_prefix(key))
 
     def delete(self, key):
-        return self.redis.delete(key)
+        return self.redis.delete(self._set_prefix(key))
 
     def list(self, pattern="*"):
-        return self.redis.keys(pattern)
+        return self.redis.keys(self._set_prefix(pattern))
+
+    def delete_all(self):
+        for k in self.list():
+            self.redis.delete(k)
